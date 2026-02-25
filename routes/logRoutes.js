716456c19180ed9447e95d885f9582479593deaa
om shinde <Shindeom052@gmail.com>
2026-02-25 23:48:59 +0530

    Added Dashboard page

--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const logController = require("../controllers/logController");
-const auth = require("../middleware/auth");
+const adminAuth = require("../middleware/adminAuthMiddleware");
 
-router.post("/client-log", auth, logController.clientLog);
+router.post("/client-log", adminAuth, logController.clientLog);
 
 module.exports = router;
